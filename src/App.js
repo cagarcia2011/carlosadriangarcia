5cdd5fa94df682343b5384d0fbd69694630831ca
carlosgarcia32 <69259477+carlosgarcia32@users.noreply.github.com>
2022-04-23 18:29:33 -0400

    Corrected logo icons. Added website greeting heading and style.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <div class="greeting-container">
+        <h1 class="menu-item">Hello.</h1>
+        <h1 class="accent menu-item">I am</h1>
+        <h1 class="accent menu-item">Carlos</h1>
+      </div>
     </div>
   );
 }
