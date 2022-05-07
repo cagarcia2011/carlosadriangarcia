c256acc80cab3553e4df9e74918f9e7675307494
carlosgarcia32 <69259477+carlosgarcia32@users.noreply.github.com>
2022-05-07 15:44:18 -0400

    Made the homepage responsive and usable with mobile devices. Reorganized homepage components and fixed some styling mistakes.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,37 +1,68 @@
-import React from 'react'
-import './home.css'
+import React from 'react';
+import './home.css';
 import { useState } from 'react';
+import useWindowDimensions from '../hooks/useWindowDimensions';
 
 function Home() {
 
+  const { width } = useWindowDimensions();
   const [IsAbout, setIsAbout] = useState(false);
   const [IsSkills, setIsSkills] = useState(false);
   const [IsProjects, setIsProjects] = useState(false);
+  const [iconState, setIconState] = useState(true)
+
+  const handleMenuClick = (e) => {
+    setIsAbout(!IsAbout);
+    setIsSkills(!IsSkills);
+    setIsProjects(!IsProjects);
+    setIconState(!iconState);
+  }
+
+  const handleMenuHoverIn = (setState) => {
+    setState(true);
+  }
+
+  const handleMenuHoverOut = (setState) => {
+    setState(false)
+  }
 
   return (
-    <div class="home-container">
-        <h1 class="menu-item" 
-        onMouseEnter={() => setIsAbout(true)} 
-        onMouseLeave={() => setIsAbout(false)}>
-          {IsAbout ? "About." : "Hello."}
-        </h1>
-        <h1 class="accent menu-item"
-        onMouseEnter={() => setIsSkills(true)}
-        onMouseLeave={() => setIsSkills(false)}>
-          {IsSkills ? "Skills." : "I am"}
-        </h1>
-        <h1 class="accent menu-item"
-        onMouseEnter={() => setIsProjects(true)}
-        onMouseLeave={() => setIsProjects(false)}>
-          {IsProjects ? "Projects." : "Carlos"}
-        </h1>
-        <div className="title-container">
-          <div className="title-wrapper">
-            <h2 className="title-item upper-title">Mechanical Engineer</h2>
-            <h2 className="title-item lower-title">Web Developer</h2>
-          </div>
+    <div className="home-container">
+      {width<=850 ? 
+      <span className="material-symbols-rounded menu-icon" onClick={() => handleMenuClick()}>
+      {iconState ? "menu" : "close"}
+      </span> : <></>}
+      <MenuItem onMouseEnter={() => handleMenuHoverIn(setIsAbout)} 
+      onMouseLeave={() => handleMenuHoverOut(setIsAbout)}
+      menuState={IsAbout} 
+      menuText={IsAbout ? "About." : "Hello."} 
+      cls="menu-item"/>
+      <MenuItem onMouseEnter={() => handleMenuHoverIn(setIsSkills)} 
+      onMouseLeave={() => handleMenuHoverOut(setIsSkills)}
+      menuState={IsSkills} 
+      menuText={IsSkills ? "Skills." : "I am"} 
+      cls="menu-item accent"/>
+      <MenuItem onMouseEnter={() => handleMenuHoverIn(setIsProjects)}
+      onMouseLeave={() => handleMenuHoverOut(setIsProjects)}
+      menuState={IsProjects} 
+      menuText={IsProjects ? "Project." : "Carlos."} 
+      cls="menu-item accent"/>
+      <div className="title-container">
+        <div className="title-wrapper">
+          <h2 className="title-item upper-title">Mechanical Engineer</h2>
+          <h2 className="title-item lower-title">Web Developer</h2>
         </div>
+      </div>
     </div>
+  )
+}
+
+const MenuItem = (props) => {
+
+  return (
+    <h1 className={props.cls} onMouseEnter={props.onMouseEnter} onMouseLeave={props.onMouseLeave}>
+        {props.menuText}
+    </h1>
   )
 }
 
---
+++ src/hooks/useWindowDimensions.js
@@ -0,0 +1,37 @@
+import { useState, useEffect } from 'react';
+
+export default function useWindowDimensions() {
+
+  const hasWindow = typeof window !== 'undefined';
+
+  function getWindowDimensions() {
+    const width = hasWindow ? window.innerWidth : null;
+    const height = hasWindow ? window.innerHeight : null;
+    return {
+      width,
+      height,
+    };
+  }
+
+  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+
+  useEffect(() => {
+
+    function getWindowDimensions() {
+      const width = hasWindow ? window.innerWidth : null;
+      return {
+        width,
+      };
+    }
+    if (hasWindow) {
+      function handleResize() {
+        setWindowDimensions(getWindowDimensions());
+      }
+
+      window.addEventListener('resize', handleResize);
+      return () => window.removeEventListener('resize', handleResize);
+    }
+  }, [hasWindow]);
+
+  return windowDimensions;
+}
