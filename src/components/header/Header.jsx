54166c9451e7608cdcf6d41251d9acd618f7b82a
carlosgarcia32 <69259477+carlosgarcia32@users.noreply.github.com>
2022-07-29 23:31:22 -0400

    Changed profile picture and mobile layout.

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
-import ME from '../../assets/Pic2.png'
+import ME from '../../assets/Pic1.png'
 import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
@@ -10,15 +10,14 @@ const Header = () => {
       <div className="container header__container" id="home">
         <h4>Hi I'm</h4>
         <h1>Carlos Garcia</h1>
-        <h4 className="text-light">Developer & Engineer</h4>
+        <h4 className="text-light">
+          Engineer / Python Dev / Web Dev
+        </h4>
+        <HeaderSocials />
         <CTA />
-
         <div className="me">
           <img src={ME} alt="Me" />
         </div>
-
-        <a href="#contact" className="scroll__down">Sroll Down</a>
-        <HeaderSocials />
       </div>
     </header>
   )
