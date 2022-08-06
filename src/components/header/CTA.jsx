6b6b2b99209e7190b692d9a82b481969fa86f1a3
carlosgarcia32 <69259477+carlosgarcia32@users.noreply.github.com>
2022-08-06 15:46:36 -0400

    Added Resume for downloading.

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-//import Resume from '../assets/resume.pdf'
+import Resume from '../assets/Carlos-Garcia-Resume-08-22.pdf'
 
 function CTA() {
     //Add download attribute to a and the Resume object to href:
     //<a href="Resume" download>Download CV</a>
   return (
     <div className="cta">
-        <a href="#contact" className="btn btn-primary">Download CV</a>
+        <a href={Resume} download className="btn btn-primary">Download CV</a>
         <a href="https://www.linkedin.com/in/carlos-a-garcia32/" target="_blank" rel="noreferrer" className="btn">Contact Me</a>
     </div>
   )
