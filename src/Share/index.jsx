d7e65e23696488582915d63b8dea04aa61c068f6
carlosgarcia32 <69259477+carlosgarcia32@users.noreply.github.com>
2023-03-18 23:49:08 -0400

    Reset project for Vite integration

--- src/Share/index.jsx
+++
@@ -1,4 +0,0 @@
-export { default as Header } from './Header'
-export { default as Footer } from './Footer'
-export { default as PageTitle } from './PageTitle'
-export { default as HomeCard } from './HomeCard'
