87cfbf355f6c0be6ab505c416552d5983f948407
carlosgarcia32 <69259477+carlosgarcia32@users.noreply.github.com>
2023-03-22 15:00:40 -0400

    Add icon shadow

--- src/components/TechIcon.tsx
+++ src/components/TechIcon.tsx
@@ -12,9 +12,9 @@ export function TechIcon({ IconElement, tooltip, animation, color } : TechIconPr
     return (
         <div className="relative group cursor-help">
             <span className={`relative text-[1.5rem]`}>
-                <IconElement color={color} className={`${animation && animation}`}/>
+                <IconElement color={color} className={`${animation && animation} drop-shadow-sm dark:drop-shadow-white`}/>
             </span>
-            <span className="absolute opacity-0 group-hover:opacity-60 bg-slate-500 text-white text-[0.75rem] left-1/2 -translate-x-1/2 group-hover:translate-y-1 p-1 w-[6rem] rounded-full transition-all duration-300 ease-in-out">{tooltip}</span>
+            <span className="absolute opacity-0 group-hover:opacity-60 bg-slate-500 text-white text-[0.75rem] left-1/2 -translate-x-1/2 group-hover:translate-y-1 p-1 w-[6rem] rounded-full transition-all duration-300 ease-in-out drop-shadow-black dark:drop-shadow-white">{tooltip}</span>
         </div>
     )
 }
