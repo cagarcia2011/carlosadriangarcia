73cb4b28a100d4951e4e230bbd20c4350bebe548
Carlos Adrian Garcia <102434180+cagarcia2011@users.noreply.github.com>
2023-03-22 02:11:42 -0400

    Update TechIcon.tsx icon color

--- src/components/TechIcon.tsx
+++ src/components/TechIcon.tsx
@@ -11,8 +11,8 @@ export function TechIcon({ IconElement, tooltip, animation, color } : TechIconPr
 
     return (
         <div className="relative group cursor-help">
-            <span className={`relative text-[${color !== null ? color : "#56b0bc"}] text-[1.5rem]`}>
-                <IconElement className={`text-[${color !== null ? color : "#56b0bc"}] ${animation && animation}`}/>
+            <span className={`relative text-[1.5rem]`}>
+                <IconElement className={`text-[color] ${animation && animation}`}/>
             </span>
             <span className="absolute opacity-0 group-hover:opacity-60 bg-slate-500 text-white text-[0.75rem] left-1/2 -translate-x-1/2 group-hover:translate-y-1 p-1 w-[6rem] rounded-full transition-all duration-300 ease-in-out">{tooltip}</span>
         </div>
