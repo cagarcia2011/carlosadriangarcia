ddbe81caaa4d3717e5e9d0054f2ab2eb039278f9
Carlos Adrian Garcia <102434180+cagarcia2011@users.noreply.github.com>
2023-03-23 02:30:43 -0400

    Update index.tsx

--- src/pages/resume/index.tsx
+++ src/pages/resume/index.tsx
@@ -27,7 +27,7 @@ export const Resume = () => {
           <div className="container sm:px-5 md:px-10 lg:px-14" data-aos="fade-up">
             <div className="py-12 px-4 md:px-0">
               {/*Resume page title */}
-              <h2 className="relative after-effect after:left-44 sm:mt-[15rem] lg:mt-0">Resume</h2>
+              <h2 className="after-effect after:left-44 mt-[15rem] lg:mt-0">Resume</h2>
               <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-x-6 gap-y-6 mt-[30px]">
                 {/* resume items map */}
                 <ResumeCard />
