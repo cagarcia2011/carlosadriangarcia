02d6887e63a1feabdec347d808ae4db644b173a3
carlosgarcia32 <69259477+carlosgarcia32@users.noreply.github.com>
2023-03-24 16:15:22 -0400

    Delete facebook link

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -5,16 +5,6 @@ export function SocialLinks() {
 
     return (
         <div className="flex justify-center space-x-3">
-            {/* facebook link add here */}
-            <a
-              href="https://www.facebook.com/carlosadrian.garcia.98/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <span className="socialbtn text-[#1773EA]">
-                <FaFacebookF />
-              </span>
-            </a>
             <a
               href="https://www.linkedin.com/in/carlos-a-garcia32/"
               target="_blank"
