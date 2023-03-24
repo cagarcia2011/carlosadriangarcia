df43bca8533691d7ddc140f56a8ec35711731354
carlosgarcia32 <69259477+carlosgarcia32@users.noreply.github.com>
2023-03-24 16:18:09 -0400

    Delete phone & bday

--- src/components/PersonalInformation.tsx
+++ src/components/PersonalInformation.tsx
@@ -5,27 +5,6 @@ export function PersonalInformation() {
 
     return (
         <div className="p-7 rounded-2xl mt-7  bg-[#F3F6F6] dark:bg-[#1D1D1D]">
-            <div className="flex border-b border-[#E3E3E3] dark:border-[#3D3A3A] pb-2.5">
-                <a href="tel:+18123618052" className="cursor-pointer">
-                    <span className="flex-shrink-0 socialbtn bg-white dark:bg-black text-accent-color shadow-md">
-                        <FaMobileAlt />
-                    </span>
-                </a>
-                <div className="text-left ml-2.5">
-                    <p className="text-xs text-[#44566C] dark:text-[#A6A6A6]">
-                        Phone
-                    </p>
-                    <p className="dark:text-white break-all">
-                        {" "}
-                        <a
-                            className="hover:text-accent-color duration-300 transition"
-                            href="tel:+18123618052"
-                        >
-                            {import.meta.env.VITE_PHONE}
-                        </a>
-                    </p>
-                </div>
-            </div>
 
             <div className="flex  border-b border-[#E3E3E3] dark:border-[#3D3A3A] py-2.5">
                 <a href="mailto:ca.garcia2011@gmail.com" className="cursor-pointer">
@@ -58,18 +37,6 @@ export function PersonalInformation() {
                         Location
                     </p>
                     <p className="dark:text-white break-all">{import.meta.env.VITE_LOCATION}</p>
-                </div>
-            </div>
-
-            <div className="flex  py-2.5">
-                <span className="flex-shrink-0 socialbtn bg-white dark:bg-black text-accent-color shadow-md">
-                    <FaCalendarAlt />
-                </span>
-                <div className="text-left ml-2.5">
-                    <p className="text-xs text-[#44566C] dark:text-[#A6A6A6]">
-                        Birthday
-                    </p>
-                    <p className="dark:text-white break-all">{import.meta.env.VITE_BDAY}</p>
                 </div>
             </div>
         </div>
