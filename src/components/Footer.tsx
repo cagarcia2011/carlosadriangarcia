92593e58552e939852f7e89485cec21d8f52f00b
carlosgarcia32 <69259477+carlosgarcia32@users.noreply.github.com>
2023-03-30 14:58:25 -0400

    Delete footer animations

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -13,7 +13,6 @@ export const Footer = () => {
     return (
         <footer
         className="overflow-hidden rounded-b-2xl bg-transparent border-t-2 border-t-accent-light dark:border-t-[#212425]"
-        data-aos="fade-up"
         >
         <p className="text-center py-6 text-gray-lite  dark:text-color-910 ">
             © 2023 All Rights Reserved by{" "}
