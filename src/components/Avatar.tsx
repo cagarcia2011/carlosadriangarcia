35597975b726a94bae0c676cad165e699dfec935
Carlos Garcia <ca.garcia2011@gmail.com>
2023-04-09 16:29:48 -0400

    Delete avatar hover effects

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -12,7 +12,7 @@ export const Avatar = ({ absolute, width } : AvatarProps) => {
     return (
         <img
           src={currentImgSelection}
-          className={`w-[200px] lg:w-[260px] ${absolute ? "absolute" : "relative"} cursor-pointer hover:scale-105 transition-all duration-200 ease-in-out left-[50%] transform -translate-x-[50%] h-auto drop-shadow-black mx-auto rounded-lg -mt-[140px] dark:drop-shadow-white`}
+          className={`w-[200px] lg:w-[260px] ${absolute ? "absolute" : "relative"} transition-all duration-200 ease-in-out left-[50%] transform -translate-x-[50%] h-auto drop-shadow-black mx-auto rounded-lg -mt-[140px] dark:drop-shadow-white`}
           alt=""
         />
     )
